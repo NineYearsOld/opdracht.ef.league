54a5b828ebad62c5b4f73fa7ea376f96a38b1fd2
Raymond Tréville <raymond.treville@student.hogent.be>
2020-10-07 12:50:25 +0200

    main

---
+++ LeagueApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeagueApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LeagueLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LeagueLibrary
+{
+    public class Class1
+    {
+    }
+}
